0a3712a2e288deac3298e82c38c02ee0899b11c3
Dolores Moriconi <dolomoriconi@gmail.com>
2024-06-19 12:52:32 -0300

    segundo commmit con la ruta que devuelve la  estructura HATEOAS y la ruta que devuelve las joyas segun categoria

--- server.js
+++ server.js
@@ -8,5 +8,50 @@ app.get('/', (req, res) => {
 })
 
 app.get("/joyas", (req, res) =>{
-  res.send(joyas)
+  const joyasRespuesta = HATEOAS(joyas.results)
+  res.send(joyasRespuesta)
 })
+
+// localhost:3000/joyas/3
+app.get("/joyas/:id", (req, res) => {
+  const { id } = req.params
+
+  const joyaResultado = joyas.results.filter(joya => joya.id == id)
+  if (joyaResultado.length) {
+    res.send(joyaResultado)
+  } else {
+    res.status(404).json({
+      message: 'Record not found',
+      status: 404
+    })
+  }
+})
+
+// localhost:3000/joyas/categoria/:categoria
+app.get("/joyas/categoria/:categoria", (req, res) => {
+  const { categoria } = req.params
+
+  const joyaResultado = joyas.results.filter(joya => joya.category == categoria)
+  if (joyaResultado.length) {
+    res.send(joyaResultado)
+  } else {
+    res.status(404).json({
+      message: 'Record not found',
+      status: 404
+    })
+  }
+})
+
+//tomar todas las joyas y crear una URL para que sea visitada
+function HATEOAS(joyas) {
+  const joyasHATEOAS = joyas.map(joya => {
+    const joyaMapeada = {
+      name: joya.name,
+      url: `http://localhost:3000/joyas/${joya.id}`
+    }
+
+    return joyaMapeada
+  })
+
+  return joyasHATEOAS
+}
